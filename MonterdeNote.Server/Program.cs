b12f03a544d19da9e93e9d2a03f3d7f84eb8ead1
Keanu Monterde <monterdekeanu@gmail.com>
2025-03-02 06:01:20 +0800

    Implemented note controller, docker compose, and sql server

--- MonterdeNote.Server/Program.cs
+++ MonterdeNote.Server/Program.cs
@@ -1,5 +1,9 @@
 using Microsoft.EntityFrameworkCore;
+using MonterdeNote.Server.Core.Repositories;
+using MonterdeNote.Server.Core.Services;
 using MonterdeNote.Server.Data;
+using MonterdeNote.Server.Repositories;
+using MonterdeNote.Server.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +13,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddScoped<INoteService, NoteService>();
+builder.Services.AddScoped<INoteRepository, NoteRepository>();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
