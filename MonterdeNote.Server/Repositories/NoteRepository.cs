baf8e362137431d588876e7602f44702b9b3f2a7
Keanu Monterde <monterdekeanu@gmail.com>
2025-03-02 04:36:06 +0800

    Initializing

---
+++ MonterdeNote.Server/Repositories/NoteRepository.cs
@@ -0,0 +1,34 @@
+﻿using MonterdeNote.Server.Core.Repositories;
+using MonterdeNote.Server.Data;
+using MonterdeNote.Server.Shared.DTOs;
+
+namespace MonterdeNote.Server.Repositories
+{
+    public class NoteRepository : INoteRepository
+    {
+        private readonly AppContext _context;
+        public NoteRepository(AppContext appContext) {
+
+            _context = appContext;
+        }
+        public Task Delete(Guid guid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task GetById(Guid guid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Post(NoteCreationDto noteCreationDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
